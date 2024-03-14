f82e230e07f22227b36abead8ef8071ae700d07d
MatthewBL <xamtehwt@gmail.com>
2024-03-14 11:57:45 +0100

    first commit

---
+++ app/src/main/java/com/example/myapplication/SplashActivity.java
@@ -0,0 +1,25 @@
+package com.example.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+public class Splash extends AppCompatActivity {
+
+    private final int DURACION=1000;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(Splash.this,MainActivity.class));
+            }
+        },DURACION);
+    }
+}
